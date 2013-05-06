4f399c8b474c52d3ff2f0715e0ad03e08df8b329
yash <yash.bathia@yahoo.com>
2013-05-06 00:53:09 -0700

    SQLite model added

--- classroom/models.py
+++ classroom/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class usercollection(models.Model):
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    firstName = models.CharField(max_length=200)
+    lastName = models.CharField(max_length=200)
+
